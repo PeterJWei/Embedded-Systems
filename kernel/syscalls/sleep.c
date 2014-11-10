1157fb3f7b9b5fd655d100f89b884351865ae0ed
Vruti <vsivakum@andrew.cmu.edu>
2014-11-10 17:31:02 -0500

    modified sleep.c

--- kernel/syscalls/sleep.c
+++ kernel/syscalls/sleep.c
@@ -1,21 +1,8 @@
 #include <types.h>
 #include <arm/reg.h>
 #include <arm/timer.h>
-#include "../interrupts/interrupts.h"
-#include "../interrupts/timers.h"
-
-//size for each overflow
-#define OVERFLOW_MS ((UINT_MAX / OSTMR_FREQ) * 1000)
 
 void sleep(unsigned long ms) {
-   uint32_t overflow = ms / OVERFLOW_MS;
-   uint32_t r = get_ticks(ms % OVERFLOW_MS);
-
-   reg_write(OSTMR_OSMR_ADDR(0), reg_read(OSTMR_OSCR_ADDR));
-   while (sleep_interrupts < overflow);
-
-   reg_write(OSTMR_OSMR_ADDR(0), reg_read(OSTMR_OSCR_ADDR) + r);
-   while(sleep_interrupts == overflows_needed);
-
-   sleep_interrupts=0;
+   unsigned long end_time = get_clock() -4 + ms
+   while (end_time!=get_clock());
 }
