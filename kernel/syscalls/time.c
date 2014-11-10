ed4c459da75fcaa51d310cea873e2ebddb651698
peter <PeterJWei1993@gmail.com>
2014-11-10 18:08:47 -0500

    added back files

---
+++ kernel/syscalls/time.c
@@ -0,0 +1,9 @@
+#include <types.h>
+#include <arm/timer.h>
+#include <arm/reg.h>
+
+#define MS_PER_OVERFLOW ((uint32_t) ((UINT_MAX /OSTMR_FREQ)*1000u))
+
+unsigned long time() {
+    return get_clock();
+}
